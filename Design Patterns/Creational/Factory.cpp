5023f578e885270855eca39d7e7abd774e8b813a
daken04 <aryan.vermaav417@gmail.com>
2024-08-30 00:45:36 +0530

    update

---
+++ Design Patterns/Creational/Factory.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Toy{
+public:
+    virtual void package() const = 0;
+};
+
+class Car : public Toy{
+public:
+    void package() const override{
+        cout<<"The Car is Packaged"<<endl;
+    }
+};
+
+class Doll : public Toy{
+public:
+    void package() const override{
+        cout<<"The Doll is Packaged"<<endl;
+    }
+};
+
+class ToyFactory{
+public:
+    Toy& createToy(string type){ // return either the reference or pointer if returning datatype is the interface
+        if(type=="Car"){
+            Car c;
+            return c;
+        }
+        else{
+            Doll d;
+            return d;
+        }
+    }
+};
+
+int main(){
+    ToyFactory f;
+    Toy& toy1 = f.createToy("Car");
+    toy1.package();
+
+    Toy& toy2 = f.createToy("Doll");
+    toy2.package();
+}
